6c31a1c68617a97b11d8550f6794ddd83f3ba629
Hayden Free <hayden.free@uky.edu>
2020-04-29 13:50:44 -0400

    createuser working

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -18,15 +18,29 @@ class ApiController < ActionController::API
     end
     
     def createuser
-        if authenticate(params[:handle], params[:password])
-            render json: {handle: params[:handle], pass: params[:password]}.to_json, status: :ok
+        # create connection to db
+        client = connect()
+        
+        begin
+            # attempt to insert new user
+            results = client.query("insert into Identity (handle, pass, fullname, location, email, bdate, joined) values (\"#{params[:handle]}\", \"#{params[:password]}\", \"#{params[:fullname]}\", \"#{params[:location]}\", \"#{params[:xmail]}\", \"#{params[:bdate]}\", \"#{Date.today.to_s}\");")            
+        rescue => exception
+            # catch and render error if there is one
+            render json:{"status":"-2", "error":"#{exception}"}.to_json
         else
-            render json: {status: "-1", error: "Authentication failed"}.to_json, status: :ok
+            # grab the id of the last inserted user
+            id = client.query("SELECT LAST_INSERT_ID();")
+            # return that id
+            render json: {"status":"#{id.first["LAST_INSERT_ID()"]}"}.to_json, status: :ok
         end
     end
 
     def seeuser
-        
+        if authenticate(params[:handle], params[:password])
+            render json: {handle: params[:handle], pass: params[:password]}.to_json, status: :ok
+        else
+            render json: {"status_code":"-10", "error":"invalid credentials"}.to_json, status: :ok
+        end        
     end
 
     def suggestions
